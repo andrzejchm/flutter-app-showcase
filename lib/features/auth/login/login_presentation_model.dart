dc52faf39c9c60ea0fcff75058caa04a8ce5d3b8
Omar Hussein <omarhusseiin@gmail.com>
2022-10-08 20:58:38 +0300

    add: LoginPresentation's implementation

--- lib/features/auth/login/login_presentation_model.dart
+++ lib/features/auth/login/login_presentation_model.dart
@@ -1,20 +1,49 @@
-import 'package:flutter_demo/features/auth/login/login_initial_params.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_demo/core/domain/model/user.dart';
+import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/features/auth/domain/model/log_in_failure.dart';
 
 /// Model used by presenter, contains fields that are relevant to presenters and implements ViewModel to expose data to view (page)
 class LoginPresentationModel implements LoginViewModel {
   /// Creates the initial state
-  LoginPresentationModel.initial(
-    // ignore: avoid_unused_constructor_parameters
-    LoginInitialParams initialParams,
-  );
+  LoginPresentationModel.initial()
+      : username = '',
+        password = '',
+        result = const FutureResult.empty();
 
   /// Used for the copyWith method
-  LoginPresentationModel._();
+  LoginPresentationModel._({
+    required this.username,
+    required this.password,
+    required this.result,
+  });
 
-  LoginPresentationModel copyWith() {
-    return LoginPresentationModel._();
+  final String username;
+  final String password;
+  final FutureResult<Either<LoginFailure, User>> result;
+
+  @override
+  bool get isLoginButtonEnabled => username.isNotEmpty && password.isNotEmpty;
+
+  @override
+  bool get isLoading => result.isPending();
+
+  LoginPresentationModel copyWith({
+    String? username,
+    String? password,
+    FutureResult<Either<LoginFailure, User>>? result,
+  }) {
+    return LoginPresentationModel._(
+      username: username ?? this.username,
+      password: password ?? this.password,
+      result: result ?? this.result,
+    );
   }
 }
 
 /// Interface to expose fields used by the view (page).
-abstract class LoginViewModel {}
+abstract class LoginViewModel {
+  bool get isLoading;
+
+  bool get isLoginButtonEnabled;
+}
--- lib/features/auth/login/login_presenter.dart
+++ lib/features/auth/login/login_presenter.dart
@@ -1,15 +1,39 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/core/utils/either_extensions.dart';
+import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
+import 'package:flutter_demo/localization/app_localizations_utils.dart';
 
 class LoginPresenter extends Cubit<LoginViewModel> {
   LoginPresenter(
-    LoginPresentationModel super.model,
+    super.model,
     this.navigator,
+    this.loginUseCase,
   );
 
   final LoginNavigator navigator;
+  final LoginUseCase loginUseCase;
 
-  // ignore: unused_element
   LoginPresentationModel get _model => state as LoginPresentationModel;
+
+  void onUsernameChanged(String text) => emit(_model.copyWith(username: text));
+
+  void onPasswordChanged(String text) => emit(_model.copyWith(password: text));
+
+  Future<void> login() {
+    return loginUseCase
+        .execute(username: _model.username, password: _model.password)
+        .observeStatusChanges(
+          (result) => emit(_model.copyWith(result: result)),
+        )
+        .asyncFold(
+          (fail) => navigator.showError(fail.displayableFailure()),
+          (success) => navigator.showAlert(
+            title: appLocalizations.logInSuccess,
+            message: appLocalizations.logInSuccessDescription,
+          ),
+        );
+  }
 }
