9383920d4b018f2dedfbc9d7dcdb42c0a380b9b1
Omar Hussein <omarhusseiin@gmail.com>
2022-10-08 20:07:35 +0300

    add: LoginRoute mixin AppInitNavigator

--- lib/features/app_init/app_init_navigator.dart
+++ lib/features/app_init/app_init_navigator.dart
@@ -1,8 +1,9 @@
+import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/navigation/app_navigator.dart';
 import 'package:flutter_demo/navigation/error_dialog_route.dart';
 import 'package:flutter_demo/navigation/no_routes.dart';
 
-class AppInitNavigator with NoRoutes, ErrorDialogRoute {
+class AppInitNavigator with NoRoutes, ErrorDialogRoute, LoginRoute {
   AppInitNavigator(this.appNavigator);
 
   @override
