4c0dcbf879b57c6a26bcd6d2052a1fa59450b8bb
Moaz Ahmed <justmeyo2025@gmail.com>
2022-11-02 09:38:30 +0200

    add: required properties to the LoginViewModel so it can be abstracted.

--- lib/features/auth/login/login_presentation_model.dart
+++ lib/features/auth/login/login_presentation_model.dart
@@ -11,10 +11,41 @@ class LoginPresentationModel implements LoginViewModel {
   /// Used for the copyWith method
   LoginPresentationModel._();
 
+  //Implemented LoginViewModel private Values
+  bool _signInButtonEnabled = false;
+  String _username = '';
+  String _password = '';
+
+  @override
+  String get password => _password;
+
+  @override
+  bool get signInButtonEnabled =>
+      _username.trim().isNotEmpty && _password.trim().isNotEmpty;
+
+  @override
+  String get username => _username;
+
   LoginPresentationModel copyWith() {
     return LoginPresentationModel._();
+  }
+
+  void signInClicked({required bool value}) {
+    _signInButtonEnabled = value;
+  }
+
+  void usernameChanged({required String user}) {
+    _username = user;
+  }
+
+  void passwordChanged({required String password}) {
+    _password = password;
   }
 }
 
 /// Interface to expose fields used by the view (page).
-abstract class LoginViewModel {}
+abstract class LoginViewModel {
+  bool get signInButtonEnabled;
+  String get username;
+  String get password;
+}
